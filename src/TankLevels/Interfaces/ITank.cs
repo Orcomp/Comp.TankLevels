423b3537bf64e75795ce8ae4a4f6dc86a050bd4a
Mikkel Riise Lund <lund.mikkel@gmail.com>
2015-07-17 19:25:26 +1000

    Adds code contracts for ITank.

--- src/TankLevels/Interfaces/ITank.cs
+++ src/TankLevels/Interfaces/ITank.cs
@@ -6,46 +6,105 @@
 
 namespace TankLevels
 {
-	using System;
-	using System.Collections.Generic;
-	using Entities;
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.Contracts;
+    using System.Linq;
+    using Entities;
 
-	/// <summary>
-	/// Interface ITank.
-	/// Defines operations on a buffer tank that has liquid put into and taken away from it at different points in time.
-	/// The buffer tank has a min and max limit.
-	/// In current naive version the tank itself does not store the history of its levels (put and take away operations) just the Min/Max values
-	/// </summary>
-	public interface ITank
-	{
-		#region Properties
+    /// <summary>
+    /// Interface ITank.
+    /// Defines operations on a buffer tank that has liquid put into and taken away from it at different points in time.
+    /// The buffer tank has a min and max limit.
+    /// In current naive version the tank itself does not store the history of its levels (put and take away operations) just the Min/Max values
+    /// </summary>
+    [ContractClass(typeof(TankContract))]
+    public interface ITank
+    {
+        #region Properties
 
-		/// <summary>
+        /// <summary>
         /// Returns predefined minimum allowed quantity for the tank.
-		/// </summary>
+        /// </summary>
         /// <value>The minimum allowed quantity for the tank.</value>
-		double MinValue { get; }
+        double MinValue { get; }
 
-		/// <summary>
+        /// <summary>
         /// Returns predefined maximum allowed quantity for the tank.
-		/// </summary>
-		/// <value>The maximum allowed quantity for the tank.</value>
-		double MaxValue { get; }
+        /// </summary>
+        /// <value>The maximum allowed quantity for the tank.</value>
+        double MaxValue { get; }
 
-		#endregion
+        #endregion
 
-		#region Methods
+        #region Methods
 
-		/// <summary>
-		/// Checks a hypothetical put or take away operation against remaining between the min and max constraints.
-		/// </summary>
-		/// <param name="startTime">The possible earlier start time of the operation.</param>
-		/// <param name="duration">The duration of the operation.</param>
-		/// <param name="quantity">The quantity to put or take away. Positive values represent a put operation while negative values are take aways.</param>
-		/// <param name="tankLevels">Tank levels are a serie of time/level pairs, and represent the Tank's level over the time.</param>
+        /// <summary>
+        /// Checks a hypothetical put or take away operation against remaining between the min and max constraints.
+        /// </summary>
+        /// <param name="startTime">The possible earlier start time of the operation.</param>
+        /// <param name="duration">The duration of the operation.</param>
+        /// <param name="quantity">The quantity to put or take away. Positive values represent a put operation while negative values are take aways.</param>
+        /// <param name="tankLevels">Tank levels are a serie of time/level pairs, and represent the Tank's level over the time.</param>
 		/// <returns>CheckOperationResult.</returns>
-		CheckOperationResult CheckOperation(DateTime startTime, TimeSpan duration, double quantity, IEnumerable<TankLevel> tankLevels);
+        CheckOperationResult CheckOperation(DateTime startTime, TimeSpan duration, double quantity, IEnumerable<TankLevel> tankLevels);
 
-		#endregion
-	}
+        #endregion
+    }
+
+    [ContractClassFor(typeof(ITank))]
+    internal abstract class TankContract : ITank
+    {
+        public double MinValue
+        {
+            get
+            {
+                // Less than MaxValue
+                Contract.Ensures(Contract.Result<double>().CompareTo(MaxValue) <= 0);
+
+                throw new NotImplementedException();
+            }
+        }
+
+        public double MaxValue
+        {
+            get
+            {
+                // Greater than MinValue
+                Contract.Ensures(MinValue.CompareTo(Contract.Result<double>()) <= 0);
+
+                throw new NotImplementedException();
+            }
+        }
+
+        public CheckOperationResult CheckOperation(DateTime startTime, TimeSpan duration, double quantity, IEnumerable<TankLevel> tankLevels)
+        {
+
+            // Can't have negative duration
+            Contract.Requires(duration.Ticks >= 0);
+
+            // All tank levels must be within the tank's limits
+            Contract.Requires(Contract.ForAll(tankLevels, tankLevel => MinValue <= tankLevel.Level && tankLevel.Level <= MaxValue));
+
+            // Tank levels have to be sorted in time
+            Contract.Requires(!tankLevels.Any() || Contract.ForAll(0, tankLevels.Count() - 1, i => tankLevels.ToArray()[i].DateTime <= tankLevels.ToArray()[i + 1].DateTime));
+
+
+            // Start time is never before first tank level
+            Contract.Ensures(!Contract.Result<CheckOperationResult>().IsSuccess
+                || tankLevels.First().DateTime.CompareTo(Contract.Result<CheckOperationResult>().StartTime) <= 0);
+
+            // End time is never after last tank level
+            Contract.Ensures(!Contract.Result<CheckOperationResult>().IsSuccess
+                || (Contract.Result<CheckOperationResult>().StartTime + duration).CompareTo(tankLevels.Last().DateTime) <= 0);
+
+            // After operation, the tank never overflows/underflows
+            Contract.Ensures(!Contract.Result<CheckOperationResult>().IsSuccess
+                || tankLevels.All(tankLevel => tankLevel.DateTime <= Contract.Result<CheckOperationResult>().StartTime + duration
+                    || MinValue <= tankLevel.Level + quantity && tankLevel.Level + quantity <= MaxValue
+                ));
+
+            throw new NotImplementedException();
+        }
+    }
 }
